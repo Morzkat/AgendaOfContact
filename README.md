a5eaf9934d49ffd467ac9c37a9a2882148db84a4
Felix David Garcia <felix.david21@outlook.com>
2017-10-17 12:43:20 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# AgendaOfContact
+# Agenda Of Contact
+A simple Agenda Of Contact made in C++
+'# Agenda Of Contact' 
+'A simple agenda of contact made in C++' 
+# Agenda Of Contact
+A simple agenda of contact made in C++
+# hello
---
+++ cmake-build-debug/CMakeFiles/AgendaOfContact.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AgendaOfContact.dir/main.cpp.obj"
+  "AgendaOfContact.pdb"
+  "AgendaOfContact.exe"
+  "AgendaOfContact.exe.manifest"
+  "libAgendaOfContact.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AgendaOfContact.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
